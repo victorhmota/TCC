cb37cf37fc1edd0d726c81e7a0c79a07bb428727
unknown <victhmota@gmail.com>
2023-07-12 16:51:02 -0300

    Obtendo dados

--- dados.R
+++ dados.R
@@ -1,6 +1,14 @@
-library(tidyverse)
-library(basedosdados)
+library("tidyverse")
+library("basedosdados")
+library('ipeadatar')
 
 
+#set_billing_id("tcc-demografia-e-crescimento")
+#query <- bdplyr(c("br_ibge_censo_demografico.microdados_domicilio_1980"))
+#df <- bd_collect(query)
 
+ipeadatar::search_series(terms = 'População total')
 
+#População
+pop <- ipeadata('ADH_PESOTOT')
+pop_mun <- pop %>% filter(pop$uname == 'Municipality')
