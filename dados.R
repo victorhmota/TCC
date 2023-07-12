435bdfc3100e648f16454ab29f1ae075088afcd7
unknown <victhmota@gmail.com>
2023-07-12 18:52:38 -0300

    Taxa de crescimento da população

--- dados.R
+++ dados.R
@@ -3,12 +3,18 @@ library("basedosdados")
 library('ipeadatar')
 
 
-#set_billing_id("tcc-demografia-e-crescimento")
-#query <- bdplyr(c("br_ibge_censo_demografico.microdados_domicilio_1980"))
-#df <- bd_collect(query)
+#Taxa de crescimento da população
+pop <- read.csv('pop.csv', skip = 1 )
+pop_mun <- pop %>% select(-c('X1996','X2007', 'X'))
 
-ipeadatar::search_series(terms = 'População total')
+cresc_pop <- pop_mun
+cresc_pop <- cresc_pop %>% mutate(X1980 = X1980/X1970 - 1, 
+                                  X1991 = X1991/pop_mun$X1980 - 1, 
+                                  X2000 = X2000/pop_mun$X1991 - 1, 
+                                  X2010 = X2010/pop_mun$X2000 - 1)
 
-#População
-pop <- ipeadata('ADH_PESOTOT')
-pop_mun <- pop %>% filter(pop$uname == 'Municipality')
+df_pop <- pivot_longer(cresc_pop, 
+                       cols = starts_with('X'), 
+                       names_to = "ano",
+                       values_to = "cresc_pop")
+
