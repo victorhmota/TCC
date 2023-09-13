65a851984fcfcf43ed929185478e303701aacacd
unknown <victhmota@gmail.com>
2023-09-12 23:20:38 -0300

    Insere alterações

--- dados.R
+++ dados.R
@@ -108,66 +108,155 @@ df_suporte2 <- capital %>% mutate(Y1970_cresc = NA,
                                   Y1991_cresc = log(X1991/X1980), 
                                   Y2000_cresc = log(X2000/X1991))
 
-df_suporte2$Y2010_cresc <- rowsMean(df_suporte2[, 10:12], rm.na = TRUE)
+df_suporte2$Y2010_cresc <- rowMeans(df_suporte2[, 10:12], na.rm = TRUE)
+capital$X2010 <- (1+df_suporte$Y2010_cresc)*capital$X2000
+capital <- capital %>% select(-X)
+
+df_capital <- pivot_longer(capital, 
+                             cols = starts_with('X'), 
+                             names_to = "ano",
+                             values_to = "capital")
+
+df_capital['ano'][df_capital['ano'] == "X1970"] <- '1970'
+df_capital['ano'][df_capital['ano'] == "X1980"] <- '1980'
+df_capital['ano'][df_capital['ano'] == "X1991"] <- '1991'
+df_capital['ano'][df_capital['ano'] == "X2000"] <- '2000'
+df_capital['ano'][df_capital['ano'] == "X2010"] <- '2010'
+
+capital_trabalho <- left_join(df_dados_pea, df_capital)
+capital_trabalho$capital_trab <- capital_trabalho$capital/capital_trabalho$pea
+
+df_suporte3 <- capital_trabalho %>% select(c('Sigla','Código', 'Município', 'ano', 'capital_trab'))
+
+df_suporte3 <- df_suporte3 %>% pivot_wider(names_from = 'ano',
+                                           values_from = 'capital_trab')
+
+colnames(df_suporte3)[4:8] <- c('X1970','X1980', 'X1991', 'X2000', 'X2010')
+
+
+cresc_cap_trab <- df_suporte3 %>% mutate(Y1970_cresc = NA,
+                                         Y1980_cresc = log(X1980/X1970), 
+                                         Y1991_cresc = log(X1991/X1980), 
+                                         Y2000_cresc = log(X2000/X1991),
+                                         Y2010_cresc = log(X2010/X2000))                      
+
+df_cresc_cap_trab <- cresc_cap_trab %>% select(-c('X1970','X1980','X1991','X2000', 'X2010')) %>% 
+                                                  pivot_longer(cols = starts_with('Y'), 
+                                                  names_to = "ano",
+                                                  values_to = "cresc_cap_trab")
+
+df_cresc_cap_trab['ano'][df_cresc_cap_trab ['ano'] == "Y1970_cresc"] <- '1970'
+df_cresc_cap_trab['ano'][df_cresc_cap_trab ['ano'] == "Y1980_cresc"] <- '1980'
+df_cresc_cap_trab['ano'][df_cresc_cap_trab ['ano'] == "Y1991_cresc"] <- '1991'
+df_cresc_cap_trab['ano'][df_cresc_cap_trab ['ano'] == "Y2000_cresc"] <- '2000'
+df_cresc_cap_trab['ano'][df_cresc_cap_trab ['ano'] == "Y2010_cresc"] <- '2010'
+
+df_capital_final <- left_join(capital_trabalho, df_cresc_cap_trab)
+
+### Escolaridade ###
+
+estados <- c("RO" = '11', "AC" = "12", 'AM' = '13', 'RO' = '14', 'PA' = '15', 'AP' = '16',
+             'TO' = '17', 'MA' = '21', 'PI' = '22', 'CE' = '23', 'RN' = '24', 'PB' = '25',
+             'PE' = '26', 'AL' = '27', 'SE' = '28', 'BA' = '29', 'MG' = '31', 'ES'= '32', 
+             'RJ' = '33','SP' = '35', 'PR' = '41', 'SC' = '42', 'RS' = '43', 'MS' = '50',
+             'MT' = '51', 'GO' = '52', 'DF' = '53')
+
+estados2 <- c('11' = "RO", '12' ="AC", '13' = 'AM', '14' = 'RO', '15' = 'PA', '16' ='AP',
+             '17' = 'TO', '21' = 'MA', '22' = 'PI', '23'= 'CE', '24' = 'RN', '25' = 'PB',
+             '26' = 'PE', '27' = 'AL', '28' = 'SE', '29' = 'BA', '31' = 'MG', '32' = 'ES', 
+             '33' = 'RJ','35'= 'SP', '41' = 'PR', '42' = 'SC', '43' = 'RS', '50' = 'MS',
+             '51' = 'MT', '52' = 'GO', '53' = 'DF')
+
+
+escolaridade <- read.csv('Dados IPEA/escolaridade.csv', skip = 1) %>% select(-X)
+dados_educ2010 <- read_dta('Dados/2010_completo.dta')
+dados_educ2000 <- read_dta('Dados/2000_completo.dta')
+
+escolaridade2010 <- dados_educ2010 %>% select(c('UF','codigo7', 'anos_est'))
+escolaridade2010$Sigla <- str_replace_all(escolaridade2010$UF, pattern = estados2)
+colnames(escolaridade2010) <- c('UF', 'Código', 'X2010', 'Sigla')
+escolaridade2010$UF <- as.character(escolaridade2010$UF)
+
+escolaridade2000 <- dados_educ2000 %>% select(c('UF','codigo7', 'anos_est'))
+escolaridade2000$Sigla <- str_replace_all(escolaridade2000$UF, pattern = estados2)
+colnames(escolaridade2000) <- c('UF', 'Código', 'X2000', 'Sigla')
+escolaridade2000$UF <- as.character(escolaridade2000$UF)
+
+escolaridade$UF <- str_replace_all(escolaridade$Sigla, pattern = estados)
+
+
+dados_escolaridade1 <- left_join(escolaridade, escolaridade2000)
+df_escolaridade <- left_join(dados_escolaridade1, escolaridade2010)
+
+df_suporte4 <- df_escolaridade %>% mutate(Y1970_cresc = NA,
+                                         Y1980_cresc = log(X1980/X1970), 
+                                         Y1991_cresc = log(X1991/X1980), 
+                                         Y2000_cresc = log(X2000/X1991),
+                                         Y2010_cresc = log(X2010/X2000)) 
+
+df_esc <- df_suporte4 %>% select(c(Sigla, 
+                                  Código, 
+                                  Município,
+                                  X1970,
+                                  X1980,
+                                  X1991, 
+                                  X2000, 
+                                  X2010))
+
+df_cresc_esc<- df_suporte4 %>% select(c(Sigla, 
+                                        Código, 
+                                        Município,
+                                        Y1970_cresc,
+                                        Y1980_cresc,
+                                        Y1991_cresc, 
+                                        Y2000_cresc, 
+                                        Y2010_cresc))
+df_esc_final <- pivot_longer(df_esc, 
+                             cols = starts_with('X'), 
+                             names_to = "ano",
+                             values_to = "escolaridade")
+
+
+df_esc_final['ano'][df_esc_final['ano'] == "X1970"] <- '1970'
+df_esc_final['ano'][df_esc_final['ano'] == "X1980"] <- '1980'
+df_esc_final['ano'][df_esc_final['ano'] == "X1991"] <- '1991'
+df_esc_final['ano'][df_esc_final['ano'] == "X2000"] <- '2000'
+df_esc_final['ano'][df_esc_final['ano'] == "X2010"] <- '2010'
+
+df_cresc_esc_final <- pivot_longer(df_cresc_esc, 
+                                   cols = starts_with('Y'), 
+                                   names_to = "ano",
+                                   values_to = "cresc_escolaridade")
+
+df_cresc_esc_final['ano'][df_cresc_esc_final['ano'] == "Y1970_cresc"] <- '1970'
+df_cresc_esc_final['ano'][df_cresc_esc_final['ano'] == "Y1980_cresc"] <- '1980'
+df_cresc_esc_final['ano'][df_cresc_esc_final['ano'] == "Y1991_cresc"] <- '1991'
+df_cresc_esc_final['ano'][df_cresc_esc_final['ano'] == "Y2000_cresc"] <- '2000'
+df_cresc_esc_final['ano'][df_cresc_esc_final['ano'] == "Y2010_cresc"] <- '2010'
+
+df_dados_esc <- left_join(df_esc_final, df_cresc_esc_final)
 
 
 
-
-capital_trabalho <- inner_join(capital, pea, by = 'Código')
-capital_trabalho <- capital_trabalho %>% mutate(k_1970 = X1970.x/X1970.y,
-                           k_1980 = X1980.x/X1980.y,
-                           k_1991 = X1991.x/X1991.y,
-                           k_2000 = X2000.x/X2000.y)
-capital_trabalho <- capital_trabalho %>% select(Sigla.x, Código, Município.x,
-                                               k_1970, k_1980, k_1991, k_2000)
-colnames(capital_trabalho) <- c('Sigla', 'Código', 'Município', 'X1970',
-                                'X1980',
-                                'X1991', 'X2000')
-
-cresc_capital <- capital_trabalho
-cresc_capital <- cresc_capital %>% mutate(X1980 = log(X1980/X1970), 
-                                      X1991 = log(X1991/capital_trabalho$X1980), 
-                                      X2000 = log(X2000/capital_trabalho$X1991))
-
-cresc_capital <- cresc_capital %>% select(-X1970)
-
-df_capital <- pivot_longer(cresc_capital, 
-                           cols = starts_with('X'), 
-                           names_to = "ano",
-                           values_to = "cresc_capital")
-#Escolaridade
-
-escolaridade <- read.csv('Dados IPEA/escolaridade.csv', skip = 1)
-
-var_escolaridade <- escolaridade
-var_escolaridade <- var_escolaridade %>% mutate(log(X1980 = X1980/X1970), 
-                                                X1991 = log(X1991/escolaridade$X1980))
-
-escolaridade <- escolaridade %>% select(-c(X1970, X))
-var_escolaridade <- var_escolaridade %>% select(-c(X, X1970))
-
-
-df_escolaridade <- pivot_longer(escolaridade, 
-                           cols = starts_with('X'), 
-                           names_to = "ano",
-                           values_to = "escolaridade")
-
-df_var_escolaridade <-  pivot_longer(var_escolaridade, 
-                                     cols = starts_with('X'), 
-                                     names_to = "ano",
-                                     values_to = "var_escolaridade")
 # População 15-64 anos
 
-rd <- read.csv('RD.csv', skip = 1)
+rd <- read.csv('Dados IPEA/RD.csv', skip = 1)
 
 rd <- rd %>% select(-X)
 
 df_wa <- rd %>% pivot_longer(cols = starts_with('X'),
                               names_to = "ano",
                               values_to = "rd") %>%
-                mutate(wa = 1/(rd+1))
+                mutate(wa = 1/(rd/100+1))
 
-#Taxa de participação
+df_wa['ano'][df_wa['ano'] == 'X1970'] <- '1970'
+df_wa['ano'][df_wa['ano'] == 'X1980'] <- '1980'
+df_wa['ano'][df_wa['ano'] == 'X1991'] <- '1991'
+df_wa['ano'][df_wa['ano'] == 'X2000'] <- '2000'
+df_wa['ano'][df_wa['ano'] == 'X2010'] <- '2010'
+
+
+### Taxa de participação ###
 
 
 df_pea <- pea %>% pivot_longer(cols = starts_with('X'),
@@ -177,37 +266,56 @@ df_pop <- pop_mun %>% pivot_longer(cols = starts_with('X'),
                                names_to = "ano",
                                values_to = "pop")
 
-df_trab_pop <- left_join(df_pop, df_pea) %>% mutate(trab_pop = pea/pop)
+df_trab_pop <- left_join(df_pop_final, df_dados_pea) %>% mutate(trab_pop = pea/pop)
 
 df_prate <- left_join(df_trab_pop, df_wa) %>% mutate(prate = trab_pop/wa)
  
-#PIB (renda)
+### Renda per capita ###
 
-renda <- read.csv('renda_per_capita.csv', skip = 1)
+renda <- read.csv('Dados IPEA/renda_per_capita.csv', skip = 1) %>% select(-X)
+renda$X1970 <- NA
+renda$X1980 <- NA
 
-cresc_renda <- renda %>% select(-X)
 
-cresc_renda <- cresc_renda %>% mutate(X2000 = log(X2000/X1991), 
-                                      X2010 = log(X2010/renda$X2000)) %>% select(-X1991)
-                                    
+renda_cresc <-  renda %>% mutate(Y1970_cresc = NA,
+                                 Y1980_cresc = log(X1980/X1970), 
+                                 Y1991_cresc = log(X1991/X1980), 
+                                 Y2000_cresc = log(X2000/X1991), 
+                                 Y2010_cresc = log(X2010/X2000)) %>% select(c(Sigla, Código, Município, Y1970_cresc, Y1980_cresc, Y1991_cresc, Y2000_cresc, Y2010_cresc))
 
-df_renda <- renda %>% select(-X) %>% pivot_longer(cols = starts_with('X'),
-                         names_to = 'ano',
-                         values_to = 'renda_per_capita')
 
-df_cresc_renda <- cresc_renda %>% pivot_longer(cols = starts_with('X'),
-                                                        names_to = 'ano',
-                                                        values_to = 'cresc_renda')
+df_renda <- pivot_longer(renda, 
+                       cols = starts_with('X'), 
+                       names_to = "ano",
+                       values_to = "renda")
 
-df_renda_final <- left_join(df_renda, df_cresc_renda)
+df_renda['ano'][df_renda['ano'] == "X1970"] <- '1970'
+df_renda['ano'][df_renda['ano'] == "X1980"] <- '1980'
+df_renda['ano'][df_renda['ano'] == "X1991"] <- '1991'
+df_renda['ano'][df_renda['ano'] == "X2000"] <- '2000'
+df_renda['ano'][df_renda['ano'] == "X2010"] <- '2010'
 
-#Base de dados final
+df_renda_cresc <- pivot_longer(renda_cresc, 
+                             cols = starts_with('Y'), 
+                             names_to = "ano",
+                             values_to = "cresc_renda")
 
-df_1 <- left_join(df_prate, df_cresc_pea)
-df_2 <- left_join(df_1, df_cresc_pop)
-df_3 <- left_join(df_2, df_capital)
-df_4 <- left_join(df_3, df_renda_final)
-df_final <- left_join(df_4, df_escolaridade)
+df_renda_cresc['ano'][df_renda_cresc['ano'] == "Y1970_cresc"] <- '1970'
+df_renda_cresc['ano'][df_renda_cresc['ano'] == "Y1980_cresc"] <- '1980'
+df_renda_cresc['ano'][df_renda_cresc['ano'] == "Y1991_cresc"] <- '1991'
+df_renda_cresc['ano'][df_renda_cresc['ano'] == "Y2000_cresc"] <- '2000'
+df_renda_cresc['ano'][df_renda_cresc['ano'] == "Y2010_cresc"] <- '2010'
+
+
+df_renda_final <- left_join(df_renda, df_renda_cresc)
+
+
+### Base de dados final ####
+
+df_1 <- left_join(df_prate, df_capital_final)
+df_2 <- left_join(df_1, df_dados_esc)
+df_final <- left_join(df_2, df_renda_final)
+
 
 #Estimação sys-GMM
 df_painel <- pdata.frame(df_final, index = c("Código", "ano"))
